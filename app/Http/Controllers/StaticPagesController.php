046ad556556da6a8d32d7147346bc843a8844f4b
qingfeng1997 <1373823008@qq.com>
2019-05-06 15:55:48 +0000

    删除微博

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -15,6 +15,7 @@ class StaticPagesController extends Controller
 		if(Auth::check()){
 			$feed_items=Auth::user()->feed()->paginate(30);
 		}
+	#	var_dump($feed_items);exit;
 		return view('static_pages/home',compact('feed_items'));
 	}
 	public function help()
--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -25,6 +25,6 @@ class UserPolicy
     public function destroy(User $currentUser,User $user)
     {
 	    //只有当前d用户拥有管理员权限且删除的用户不是自己时才显示链接。
-	    return $currentUser->is_admin && $currentUser->id !==$user->id;
+	    return $currentUser->is_admin && $currentUser->id !== $user->id;
     }
 }
