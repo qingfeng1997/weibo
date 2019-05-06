65788e92ec626d06154cc836ab724f0557db4fae
qingfeng1997 <1373823008@qq.com>
2019-05-06 09:54:01 +0000

    首页微博列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,5 +56,11 @@ class User extends Authenticatable
     {
 	return $this->hasMany(Status::class);
     }
+    //获取当前用户关注的人发布过的所有微博动态
+    public function feed()
+    {
+	return $this->statuses()
+		->orderBy('created_at','desc');
+    }
 
 }
