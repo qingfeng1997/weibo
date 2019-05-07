498d51850ca6db0c5a0f882281ffba8f1b9df2e0
qingfeng1997 <1373823008@qq.com>
2019-05-07 08:10:06 +0000

    社交统计信息

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,9 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+	    
 	 Model::unguard();
-	 $this->call(StatusesTableSeeder::class);
+
 	 $this->call(UsersTableSeeder::class);
+	 $this->call(StatusesTableSeeder::class);
+	 $this->call(FollowersTableSeeder::class);
+
 	 Model::reguard();
     }
 }
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,6 +6,9 @@
   <div class="offset-md-2 col-md-8">
     <section class="user_info">
       @include('shared._user_info', ['user' => $user])
+    </section> 
+    <section class="user_info">
+      @include('shared._stats', ['user' => $user])
     </section>
     <section class="status">
       @if ($statuses->count() > 0)
