c036c1d81dd2b24ec02fa49c172aeeeac5474528
qingfeng1997 <1373823008@qq.com>
2019-05-07 09:51:17 +0000

    关注按钮

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -7,24 +7,29 @@ use Illuminate\Auth\Access\HandlesAuthorization;
 /*用户授权策略类*/
 class UserPolicy
 {
-    use HandlesAuthorization;
+	use HandlesAuthorization;
 
-    /**
-     * Create a new policy instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-    public function update(User $currentUser,User $user)
-    {
-	    return $currentUser->id===$user->id;
-    }
-    public function destroy(User $currentUser,User $user)
-    {
-	    //只有当前d用户拥有管理员权限且删除的用户不是自己时才显示链接。
-	    return $currentUser->is_admin && $currentUser->id !== $user->id;
-    }
+	/**
+	 * Create a new policy instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		//
+	}
+	public function update(User $currentUser,User $user)
+	{
+		return $currentUser->id===$user->id;
+	}
+	public function destroy(User $currentUser,User $user)
+	{
+		//只有当前d用户拥有管理员权限且删除的用户不是自己时才显示链接。
+		return $currentUser->is_admin && $currentUser->id !== $user->id;
+	}
+	//关注权限设定，自己不能关注自己
+	public function follow(User $currentUser,User $user)
+	{
+		return $currentUser->id !==$user->id;
+	}
 }
